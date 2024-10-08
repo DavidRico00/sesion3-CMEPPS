837a0185cb61e377d3655cc67d6b09f9265b2eef
David Rico <davidriico00@gmail.com>
2024-10-08 11:12:14 +0200

    1. Escribo una prueba que falle

--- src/test/CuentaTest.java
+++ src/test/CuentaTest.java
@@ -12,7 +12,7 @@ import sesion.Cuenta;
 
 class CuentaTest {
 	
-	private Cuenta cuenta;
+	private Cuenta cuenta1, cuenta2;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -24,27 +24,46 @@ class CuentaTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		cuenta = new Cuenta(0d);
+		//cuenta = new Cuenta(0d);
+		cuenta1 = new Cuenta(50d, "12345", "Pepe");
+		cuenta2 = new Cuenta(0d, "67890", "Juan");
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 	}
 
+	/*
 	@Test
 	void testIngresar() {
-		
 		cuenta.ingresar(500d);
 		assertEquals(500, cuenta.getSaldo());
-		
 	}
 	
 	@Test
 	void testRetirar() {
-		
 		cuenta.retirar(500d);
 		assertEquals(-500, cuenta.getSaldo());
+	}
+	*/
+	
+	@Test
+	void test0014() {
 		
+		cuenta1.retirar(200d);
+		cuenta2.retirar(350d);
+		cuenta1.ingresar(100d);
+		cuenta2.retirar(200d);
+		cuenta2.retirar(150d);
+		cuenta1.retirar(200d);
+		cuenta2.ingresar(50d);
+		cuenta2.ingresar(100d);
+		
+		System.out.println("Cuenta 12345: "+cuenta1.getSaldo());
+		System.out.println("Cuenta 67890: "+cuenta2.getSaldo());
+		
+		assertEquals(-250, cuenta1.getSaldo());
+		assertEquals(-450, cuenta2.getSaldo());
 	}
 
 }
