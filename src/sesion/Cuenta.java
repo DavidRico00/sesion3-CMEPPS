da4895a05cd3194611a233840d58150f5a4cb5a4
David Rico <davidriico00@gmail.com>
2024-10-10 13:11:02 +0200

    commit

--- src/sesion/Cuenta.java
+++ src/sesion/Cuenta.java
@@ -3,11 +3,18 @@ package sesion;
 public class Cuenta {
 	
 	private Double saldo;
+	private String nombre, numero;
 	
 	public Cuenta(Double i) {
 		this.saldo = i;
 	}
 	
+	public Cuenta(Double d, String numero, String nombre) {
+		this.saldo = d;
+		this.numero = numero;
+		this.nombre = nombre;
+	}
+
 	public Double getSaldo() {
 		return saldo;
 	}
